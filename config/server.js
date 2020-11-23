bcd2c791dc6622ffc1d8391fe16fd1ddbec42f0b
ericsantos <eric@exactaworks.com.br>
2020-11-23 10:56:39 -0300

    feat: create data structures

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3dcf3b20c9684ef23776b464a498da32'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '779e9672-3860-4e01-b9e8-7167d5a7158b'
+};
