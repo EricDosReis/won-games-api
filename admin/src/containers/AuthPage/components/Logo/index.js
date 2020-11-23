5f4ef60e3b8c26a77428cba1a54f37000c48820a
ericsantos <eric@exactaworks.com.br>
2020-11-23 15:14:46 -0300

    feat: change Logo component

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoImg from "../../../../assets/images/logo-won-dark.svg";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoImg} alt="Won Games" />;
+
+export default Logo;
